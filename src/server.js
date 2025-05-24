86f7dacae2af2de2f1d6b7f09367486a4ee45db5
George Machaidze <wakanichi13@gmail.com>
2025-05-24 16:06:54 +0400

    Feat: connected database

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 import cors from "cors";
 import userRouter from "./routes/user-router.js";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import connectToDatabase from "./config/mongo.js";
+
+dotenv.config();
+connectToDatabase();
 
 const app = express();
 app.use(cors());
